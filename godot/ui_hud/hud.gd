cea1cd58c2390cbda77d713fe0a64e1f5a5f3594
saltyswan <wannab.cassio@gmail.com>
2026-01-31 14:26:57 +0100

    Architecture updated

---
+++ godot/ui_hud/hud.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+
+#NOTE: 
+#Add maestro's satisfaction gauge
+#Link to score system (global variable?)
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _process(delta: float) -> void:
+	pass
