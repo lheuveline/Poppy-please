c6394716e693c9f457a0ffd7992ae95564f4b6f2
saltyswan <wannab.cassio@gmail.com>
2026-01-31 17:01:23 +0100

    Minor cleaning merge conflict

--- godot/scripts/debug_manager.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-func _ready() -> void:
-	if OS.is_debug_build():
-		print("[DEBUG] Running in debug mode (all the debug tools are available)")
-		DebugMenu.style = DebugMenu.Style.VISIBLE_DETAILED
-	else:
-		DebugMenu.style = DebugMenu.Style.HIDDEN
