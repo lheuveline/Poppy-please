44af4f68cdb302fb35b12662dd7e2b635cb1e02b
saltyswan <wannab.cassio@gmail.com>
2026-02-01 16:06:31 +0100

    Trying out stuff (and failing)

--- godot/levels/level_1/level_1.gd
+++ godot/levels/level_1/level_1.gd
@@ -1,14 +1,14 @@
 extends Node2D
 var musician_list : Array
+@onready var hud_scene = "res://ui/hud.tscn"
 
 func _ready() -> void:		
 	var char_nodes = get_tree().get_nodes_in_group("characters")
 	for node in char_nodes:
 		musician_list.append(node)
 		print("[level_1] List of musician nodes: ", musician_list)
-	#musician_list.pick_random()
-	 #switch_track()
-
-
+	var impostor = musician_list.pick_random()
+	#impostor.control_mask.switch_tracks()
+	
 func _process(_delta: float) -> void:
 	pass
