b8f6ffb392952eb6afa28392ee23f1d9d8dd9313
geekymoose <constantin.masson@umontreal.ca>
2026-02-01 16:41:40 +0100

    Fix warning in the welcome menu

--- godot/levels/start_menu/main_menu.gd
+++ godot/levels/start_menu/main_menu.gd
@@ -1,19 +1,7 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://levels/level_1/level_1.tscn")
-
 
 func _on_credits_pressed() -> void:
 	get_tree().change_scene_to_file("res://levels/credits/credits.tscn")
