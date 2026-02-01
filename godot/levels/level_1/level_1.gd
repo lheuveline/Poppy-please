e97fd191f42d2bb9fbcfc483a62601e5dd4f2569
Remi Papillie <remi.papillie@gmail.com>
2026-02-01 17:12:04 +0100

    Fix impostor initialization

--- godot/levels/level_1/level_1.gd
+++ godot/levels/level_1/level_1.gd
@@ -1,14 +1,14 @@
 extends Node2D
-var musician_list : Array
+var musician_list: Array
 @onready var hud_scene = "res://ui/hud.tscn"
 
-func _ready() -> void:		
+func _ready() -> void:
 	var char_nodes = get_tree().get_nodes_in_group("characters")
 	for node in char_nodes:
 		musician_list.append(node)
 		print("[level_1] List of musician nodes: ", musician_list)
 	var impostor = musician_list.pick_random()
-	#impostor.control_mask.switch_tracks()
+	impostor.get_node("Mask").switch_track()
 	
 func _process(_delta: float) -> void:
 	pass
