fab9f53ac602382d4cffc022fad669c30559658e
geekymoose <constantin.masson@umontreal.ca>
2026-02-04 00:58:44 +0100

    Add leave button to pause menu
    Adds a button with cross icon that exits to the main menu when pressed.

--- godot/levels/pause_menu/pause_menu.gd
+++ godot/levels/pause_menu/pause_menu.gd
@@ -29,6 +29,9 @@ func _on_restart_pressed():
 	resume()
 	get_tree().reload_current_scene()
 
+func _on_leave_pressed() -> void:
+	get_tree().change_scene_to_file("res://levels/start_menu/main_menu.tscn")
+
 func _process(_delta):
 	if Input.is_action_just_pressed("esc"):
 		toggle()
