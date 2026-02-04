7fcfcbc453542b1913be31199180a1d314038e47
root <root@DESKTOP-08U3F5T>
2026-02-04 12:45:32 +0100

    Added main theme, UI start menu effects

--- godot/levels/start_menu/main_menu.gd
+++ godot/levels/start_menu/main_menu.gd
@@ -4,10 +4,14 @@ func _ready():
 	$VBoxContainer/Start.grab_focus()
 
 func _on_start_pressed() -> void:
+	$Audio/StartKeyPressed.play()
 	get_tree().change_scene_to_file("res://levels/level_1/level_1.tscn")
 
 func _on_credits_pressed() -> void:
+	$Audio/CreditsKeyPressed.play()
 	get_tree().change_scene_to_file("res://levels/credits/credits.tscn")
 
 func _on_quit_pressed() -> void:
+	$Audio/QuitKeyPressed.play()
+	await get_tree().create_timer(0.3).timeout
 	get_tree().quit()
