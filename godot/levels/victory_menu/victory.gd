727050632320d86af4ebfaa3ec67051d91e124b1
geekymoose <constantin.masson@umontreal.ca>
2026-02-04 02:19:14 +0100

    Fix the issue that caused the victory effect to be bypassed

--- godot/levels/victory_menu/victory.gd
+++ godot/levels/victory_menu/victory.gd
@@ -2,16 +2,15 @@ extends CanvasLayer
 
 var game_won = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	GlobalData.game_won.connect(on_game_won)
 
 func on_game_won():
-	game_won = true
+	print("[VictoryMenu] Game is won")
 	$AnimationPlayer.play("win_animation")
-	print("[New victory scene] Game is won")
+	await $AnimationPlayer.animation_finished
+	game_won = true
 
-func _input(event: InputEvent) -> void:
-	if event is InputEvent and game_won:
-			if Input.is_action_pressed("throw_mask"):
-				get_tree().change_scene_to_file("res://levels/start_menu/main_menu.tscn")
+func _process(_delta: float) -> void:
+	if game_won && Input.is_action_pressed("throw_mask"):
+		get_tree().change_scene_to_file("res://levels/start_menu/main_menu.tscn")
