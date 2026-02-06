621c3a8f276a8e02bfe36ccee4ad3171843d8e27
geekymoose <constantin.masson@umontreal.ca>
2026-02-06 20:14:34 +0100

    Fix tutorial scene to prevent input spamming during animations

--- godot/scenes/tutorial/tutorial.gd
+++ godot/scenes/tutorial/tutorial.gd
@@ -1,15 +1,16 @@
 extends Node
 
 @onready var anim_player = $AnimationPlayer
-var continue_requested = false # To avoid issues if spamming the input
 
 func _ready() -> void:
 	assert(anim_player)
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("throw_mask"):
-		if !continue_requested:
-			continue_requested = true
-			anim_player.play("leave")
-			await anim_player.animation_finished
-			get_tree().change_scene_to_file.call_deferred("res://scenes/level_1/level_1.tscn")
+		if anim_player.is_playing():
+			# The "enter" or "leaving" anim is already playing
+			# Note: we cannot leave when the "enter" is still playing
+			return
+		anim_player.play("leave")
+		await anim_player.animation_finished
+		get_tree().change_scene_to_file.call_deferred("res://scenes/level_1/level_1.tscn")
